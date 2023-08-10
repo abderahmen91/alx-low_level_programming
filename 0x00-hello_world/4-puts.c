0912344425ec975a3d25e30e95c8a16103094f16
abderahmen91 <abderahmen_ksentini@outlook.fr>
2023-08-10 12:46:05 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description : C program that prints put function
+ *
+ * return : always 0 (success)
+ *
+*/
+
+int main() 
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
