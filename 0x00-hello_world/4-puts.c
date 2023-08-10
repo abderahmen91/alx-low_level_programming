117fed2176740274f4619585e664baf649007bd4
abderahmen91 <abderahmen_ksentini@outlook.fr>
2023-08-10 13:02:40 -0700

    puttC

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
  * description : C program that prints put function
  *
  * return : always 0 (success)
  *
 */
-
 int main(void) 
 {
-
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
