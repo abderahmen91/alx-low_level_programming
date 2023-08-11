80b30d2eaf68a22d3e33a73924820f82ad316544
abderahmen91 <abderahmen_ksentini@outlook.fr>
2023-08-11 09:07:26 -0700

    numbers10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - all base 10 starting from zero
+ * Return: 0 when success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
