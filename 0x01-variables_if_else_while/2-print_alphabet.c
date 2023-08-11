f5d3573f65fe15f6d506b191f8787426a4aaf26c
abderahmen91 <abderahmen_ksentini@outlook.fr>
2023-08-11 08:48:14 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 when success
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
